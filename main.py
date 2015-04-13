b6accc7d6b53f0befb4994ea682fc74fa5d29fb4
Mathurshan <mathurshan.vimalesvaran@tufts.edu>
2015-04-13 16:36:34 -0400

    setting up server

---
+++ main.py
@@ -0,0 +1,20 @@
+import webapp2
+import logging
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+    ], debug=True)
