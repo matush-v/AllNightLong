32ca0de4a6b1b7ad3afb653e3bd665b1422d2b01
Mathurshan <mathurshan.vimalesvaran@tufts.edu>
2015-04-26 22:33:46 -0400

    im tired btn image modal fixed

--- js/events.js
+++ js/events.js
@@ -266,7 +266,8 @@ function notify_user(title, icon, short_message, long_description) {
             keyboard: false
         });
 
-        $('#event_modal').find('.modal-header').prepend("<div class='modal-icon'><img src='" + icon + "' alt='event icon'>");
+        $('#event_modal').find('#modal-image').empty()
+        $('#event_modal').find('#modal-image').append("<div class='modal-icon'><img src='" + icon + "' alt='event icon'>");
         $('#event_modal').find('.modal-title').text(title);
         $('#event_modal').find('.modal-body').append(long_description);
         $('#event_modal').modal('show');
@@ -289,8 +290,9 @@ $('#tired_btn').click(function() {
     rand *= extras.length;
     rand = Math.floor(rand);
     var choice = extras[rand];
-    
-    $('#event_modal').find('.modal-header').prepend("<div class='modal-icon'><img src='" + choice.icon + "' alt='event icon'></div>");
+
+    $('#event_modal').find('#modal-image').empty()
+    $('#event_modal').find('#modal-image').append("<div class='modal-icon'><img src='" + choice.icon + "' alt='event icon'></div>");
     $('#event_modal').find('.modal-title').text(choice.name);
     $('#event_modal').find('.modal-body').append(choice.description);
     $('#event_modal').modal('show');
