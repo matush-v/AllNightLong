2b5edf2ca3a23ef744522ce595e5ccc48d053517
Mathurshan <mathurshan.vimalesvaran@tufts.edu>
2015-10-24 02:39:16 -0400

    rating modal

--- js/events.js
+++ js/events.js
@@ -375,8 +375,6 @@ function notify_user(title, event) {
         $("#event_" + event_index).remove();
 
     };
-
-
     
     // Notification is set for next event in the queue
     set_up_notification();
@@ -408,14 +406,40 @@ function show_modal(modal_name, event_icon, event_name, event_description, event
 *
 ******************************************************************************/
 function set_up_quotes() {
-    QUOTES = ['With the new day comes new strength and new thoughts.',
-              'In the middle of every difficulty lies opportunity.',
-              'All we have to decide is what to do with the time that is given us.',
-              'Don&#39;t let the muggles get you down.',
-              'Good things come to people who wait, but better things come to those who go out and get them.',
-              'If you do what you always did, you will get what you always got.',
-              'If you&#39;re going through hell keep going.',
-              'No masterpiece was ever created by a lazy artist.'];
+    QUOTES = ['With the new day comes new strength and new thoughts. - someone clever',
+              'In the middle of every difficulty lies opportunity. - someone clever',
+              'All we have to decide is what to do with the time that is given us. - someone clever',
+              'Don&#39;t let the muggles get you down. - someone clever',
+              'Good things come to people who wait, but better things come to those who go out and get them. - someone clever',
+              'If you do what you always did, you will get what you always got. - someone clever',
+              'If you&#39;re going through hell keep going. - someone clever',
+              'No masterpiece was ever created by a lazy artist. - someone clever',
+              'Life is about making an impact, not making an income. –Kevin Kruse',
+              'Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill',
+              'Strive not to be a success, but rather to be of value. –Albert Einstein',
+              'Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  –Robert Frost',
+              'I attribute my success to this: I never gave or took any excuse. –Florence Nightingale',
+              'You miss 100% of the shots you don’t take. –Wayne Gretzky',
+              'I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning t and missed. I’ve failed over and over and over again in my life. And that is why I succeed. –Michael Jordan',
+              'The most difficult thing is the decision to act, the rest is merely tenacity. –Amelia Earhart',
+              'Every strike brings me closer to the next home run. –Babe Ruth',
+              'Definiteness of purpose is the starting point of all achievement. –W. Clement Stone',
+              'Life isn’t about getting and having, it’s about giving and being. –Kevin Kruse',
+              'Life is what happens to you while you’re busy making other plans. –John Lennon',
+              'We become what we think about. –Earl Nightingale',
+              'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off  bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover. –Mark Twain',
+              'Life is 10% what happens to me and 90% of how I react to it. –Charles Swindoll',
+              'The most common way people give up their power is by thinking they don’t have any. –Alice Walker',
+              'The mind is everything. What you think you become.  –Buddha',
+              'The best time to plant a tree was 20 years ago. The second best time is now. –Chinese Proverb',
+              'An unexamined life is not worth living. –Socrates',
+              'Eighty percent of success is showing up. –Woody Allen',
+              'Your time is limited, so don’t waste it living someone else’s life. –Steve Jobs',
+              'Winning isn’t everything, but wanting to win is. –Vince Lombardi',
+              'I am not a product of my circumstances. I am a product of my decisions. –Stephen Covey',
+              'Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso',
+              'You can never cross the ocean until you have the courage to lose sight of the shore. –Christopher Columbus'
+             ] ;
 
     cur_quote = 0; // Global counter for quote list index
 
